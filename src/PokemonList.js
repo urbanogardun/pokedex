03184eda015144b1aefd818ec1c4d8a5082c0bdf
Urbano Gardun <ugardun@gmail.com>
2018-03-12 11:58:49 +0100

    Add infinite scrolling for pokemon list

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -1,10 +1,69 @@
 import React from 'react';
 import { Pokedex } from './utils/Pokedex';
 
+
+function debounce(fn, delay) {
+    var timer = null;
+    return function () {
+        var context = this, args = arguments;
+        clearTimeout(timer);
+        timer = setTimeout(function () {
+            fn.apply(context, args);
+        }, delay);
+    };
+}
+
+function getNextPageOffset(nextPageUrl) {
+    let nextPageOffset = nextPageUrl.split('=');
+    nextPageOffset = nextPageOffset[nextPageOffset.length - 1];
+    return nextPageOffset;
+}
+
 export default class PokemonList extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {pokemonList: []}
+        this.state = {pokemonList: [], pageNumber: 1, nextPageOffset: 11}
+        // this.trackScrolling = debounce(this.trackScrolling, 2000);
+    }
+    
+    isBottom(el) {
+        return el.getBoundingClientRect().bottom <= window.innerHeight;
+    }
+
+    trackScrolling = debounce(() => {
+        const wrappedElement = document.getElementById('pokemon-list');
+        if (this.isBottom(wrappedElement)) {
+            console.log('pokemon-list bottom reached');
+            this.loadMorePokemons();
+        }
+    }, 1000);
+
+    loadMorePokemons() {
+        let interval = {
+            limit: 10,
+            offset: this.state.nextPageOffset
+        }
+
+        let self = this;
+        Pokedex.getPokemonsList(interval)
+        .then(function (response) {
+            let nextPageOffset = getNextPageOffset(response.next);
+
+            // Append more pokemons
+            self.setState({
+                pokemonList: [...self.state.pokemonList, ...response.results],
+                nextPageOffset: nextPageOffset
+            })
+        });
+        document.addEventListener('scroll', this.trackScrolling);
+    }
+
+    componentDidMount() {
+        document.addEventListener('scroll', this.trackScrolling);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('scroll', this.trackScrolling);
     }
 
     componentWillMount() {
@@ -28,7 +87,7 @@ export default class PokemonList extends React.Component {
         );
 
         return (
-            <div>
+            <div id="pokemon-list">
                 <h1>Hello, {this.props.name}</h1>
 
                 {pokemons}
