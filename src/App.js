5b94ba6395dae002df54d46e3f3f0476a90790ed
Urbano Gardun <ugardun@gmail.com>
2018-03-12 10:30:56 +0100

    Fetch data from pokeapi.co

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+const Pokedex = require('pokeapi-js-wrapper');
+const options = {
+  protocol: 'https',
+  versionPath: '/api/v2/',
+  cache: true,
+  timeout: 5 * 1000 // 5s
+}
+const P = new Pokedex.Pokedex(options);
 
 class App extends Component {
   render() {
+
+    var interval = {
+      limit: 10,
+      offset: 34
+    }
+    P.getPokemonsList(interval)
+    .then(function(response) {
+      console.log(response);
+    });
+
     return (
       <div className="App">
         <header className="App-header">
