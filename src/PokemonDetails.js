77815e64688bff1b1e4ec8c090f43270d07c3f4e
Urbano Gardun <ugardun@gmail.com>
2018-03-12 14:20:32 +0100

    Render pokemon details

--- src/PokemonDetails.js
+++ src/PokemonDetails.js
@@ -25,9 +25,74 @@ export default class PokemonDetails extends React.Component {
     render() {
         const pokemon = this.state.pokemonData;
 
+        // const pokemons = this.state.pokemonList.map((link) =>
+        //     <Link 
+        //     key={link.url} 
+        //     to={`/pokemon/${link.name}`}><li>{link.name}</li></Link>
+        // );
+
+        let abilities = <div/>
+        let moves = <div/>
+        let types = <div />
+        let stats = <div />
+        let pokemonPhotoFront = <div />
+        let pokemonPhotoBack = <div />
+        if (pokemon) {
+            abilities = pokemon.abilities.map((item) =>
+                <li key={item.ability.url}>{item.ability.name}</li>
+            );
+
+            moves = pokemon.moves.map((item) =>
+                <li key={item.move.url}>{item.move.name}</li>
+            );
+
+            types = pokemon.types.map((item) =>
+                <li key={item.type.url}>{item.type.name}</li>
+            );
+        
+            // pokemon.stats.map((item) => {
+            //     console.log(item);
+            // });
+
+            stats = pokemon.stats.map((item) =>
+                <li 
+                key={item.stat.url}
+                data-base-stat={item.base_stat}
+                data-effort={item.effort}
+                >{item.stat.name}</li>
+            );
+
+            pokemonPhotoFront = <img src={pokemon.sprites.front_default} />
+            pokemonPhotoBack = <img src={pokemon.sprites.back_default} />
+        }
+
         return (
             <React.Fragment>
-                <h1>{pokemon.name}</h1>
+                <h1 className="pokemon-name">{pokemon.name}</h1>
+
+                {pokemonPhotoFront}
+                {pokemonPhotoBack}
+
+                <hr />
+
+                <p>Sposobnosti</p>
+                {abilities}
+
+                <hr />
+
+                <p>Potezi</p>
+                {moves}
+
+                <hr />
+
+                <p>Vrsta</p>
+                {types}
+
+                <hr />
+
+                <p>Statistike</p>
+                {stats}
+
             </React.Fragment>
         )
     }
