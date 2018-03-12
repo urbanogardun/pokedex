0d43314018dae187b353e5d4da2a851dcc8f25a6
Urbano Gardun <ugardun@gmail.com>
2018-03-12 18:08:01 +0100

    Render pokemons in my list

--- src/MyPokemonList.js
+++ src/MyPokemonList.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import RemovePokemonFromList from './RemovePokemonFromList';
+import { localforage } from './utils/localforageSetup';
+import { Link } from 'react-router-dom'
 
 export default class MyPokemonList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {pokemonList: []}
+    }
+
+    componentWillMount() {
+        localforage.getItem('pokemons')
+        .then((result) => {
+            this.setState({
+                pokemonList: result
+            });
+        })
+    }
+
     render() {
+        const pokemons = this.state.pokemonList.map((pokemon) =>
+            <React.Fragment key={pokemon}>
+                <Link 
+                to={`/pokemon/${pokemon}`}><li>{pokemon}</li></Link>
+
+                <RemovePokemonFromList name={pokemon} />
+            </React.Fragment>
+        );
+
         return (
-            <h1>My Pokemon List Goes Here!</h1>
+            <React.Fragment>
+                <h1>My Pokemon List Goes Here!</h1>
+    
+                <hr />
+                {pokemons}
+            </React.Fragment>
         );
     }
 }
