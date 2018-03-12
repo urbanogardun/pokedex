a4fafb48c8b40ac842cf5b5b7a2d8f1db64a6606
Urbano Gardun <ugardun@gmail.com>
2018-03-12 10:59:08 +0100

    Render a list of pokemons

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -2,18 +2,37 @@ import React from 'react';
 import { Pokedex } from './utils/Pokedex';
 
 export default class PokemonList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {pokemonList: []}
+    }
+
     componentWillMount() {
         var interval = {
-        limit: 10,
-        offset: 14
+            limit: 10,
+            offset: 0
         }
+        var self = this;
         Pokedex.getPokemonsList(interval)
-        .then(function(response) {
-          console.log(response);
+        .then(function (response) {
+            // Append more pokemons
+            self.setState({
+                pokemonList: [...self.state.pokemonList, ...response.results]
+            })
         });
     }
 
     render() {
-        return <h1>Hello, {this.props.name}</h1>;
+        const pokemons = this.state.pokemonList.map((link) =>
+            <li key={link.url}>{link.name}</li>
+        );
+
+        return (
+            <div>
+                <h1>Hello, {this.props.name}</h1>
+
+                {pokemons}
+            </div>
+        );
     }
 }
