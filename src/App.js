e1fb376fee17210f8ec201ef6c7372885aa9edf4
Urbano Gardun <ugardun@gmail.com>
2018-03-12 19:01:51 +0100

    Replace bootstrap with material-ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import {  BrowserRouter as Router, Route } from 'react-router-dom'
-import './App.css';
 import PokemonList from './PokemonList';
 import PokemonDetails from './PokemonDetails';
 import MyPokemonList from './MyPokemonList';
+import NavigationAppBar from './NavigationAppBar';
 
 class App extends Component {
   render() {
@@ -11,6 +11,7 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
+          <NavigationAppBar />
           <Route exact path="/" component={PokemonList} />
           <Route exact path="/pokemon/:name" component={PokemonDetails} />
           <Route exact path="/list/" component={MyPokemonList} />
---
+++ src/NavigationAppBar.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from 'material-ui-icons/Menu';
+import Menu, { MenuItem } from 'material-ui/Menu';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flex: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+class NavigationAppBar extends React.Component {
+  state = {
+    auth: true,
+    anchorEl: null,
+  };
+
+  handleChange = (event, checked) => {
+    this.setState({ auth: checked });
+  };
+
+  handleMenu = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { auth, anchorEl } = this.state;
+    const open = Boolean(anchorEl);
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton 
+            aria-owns={open ? 'menu-appbar' : null}
+            aria-haspopup="true"
+            onClick={this.handleMenu}
+            className={classes.menuButton} 
+            color="inherit" 
+            aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="title" color="inherit" className={classes.flex}>
+              Pokedex - Skupi Sve!
+            </Typography>
+            {auth && (
+              <div>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={open}
+                  onClose={this.handleClose}
+                >
+                  <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+                  <MenuItem onClick={this.handleClose}>My account</MenuItem>
+                </Menu>
+              </div>
+            )}
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(NavigationAppBar);
