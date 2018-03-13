3175b076e8dce57d2b6a47b3f2270df45402dbc1
Urbano Gardun <ugardun@gmail.com>
2018-03-13 13:19:32 +0100

    Sticky appbar

--- src/NavigationAppBar.js
+++ src/NavigationAppBar.js
@@ -46,7 +46,7 @@ class NavigationAppBar extends React.Component {
 
     return (
       <div className={classes.root}>
-        <AppBar position="static">
+        <AppBar position="fixed">
           <Toolbar>
             <IconButton 
             aria-owns={open ? 'menu-appbar' : null}
