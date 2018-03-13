c0d9710461f4f4cbf2460cf36df7af5906b3a3fe
Urbano Gardun <ugardun@gmail.com>
2018-03-13 09:53:25 +0100

    Refactor my pokemon list

--- src/MyPokemonList.js
+++ src/MyPokemonList.js
@@ -1,27 +1,41 @@
 import React from 'react';
-import RemovePokemonFromList from './RemovePokemonFromList';
 import { localforage } from './utils/localforageSetup';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import MyPokemonCheckbox from './MyPokemonCheckbox';
+import { withStyles } from 'material-ui/styles';
+import List, { ListItem, ListItemSecondaryAction, ListItemText } from 'material-ui/List';
+import Avatar from 'material-ui/Avatar';
 
-export default class MyPokemonList extends React.Component {
+const styles = theme => ({
+    root: {
+        width: '100%',
+        maxWidth: 360,
+        backgroundColor: theme.palette.background.paper,
+    },
+});
+
+class MyPokemonList extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {pokemonList: []}
+        this.state = {pokemonList: [], checked: []}
         this.updateMyListAfterRemovingAPokemon = this.updateMyListAfterRemovingAPokemon.bind(this);
     }
 
     componentWillMount() {
         localforage.getItem('pokemons')
         .then((result) => {
-            result = result === null ? [] : result
+            console.log(result);
+            result = result === null ? [] : result;
+            let namesOfPokemonsInMyList = result === null ? [] : result.map((val) => { return val.name })
             this.setState({
-                pokemonList: result
+                pokemonList: result,
+                checked: namesOfPokemonsInMyList
             });
         })
     }
 
     updateMyListAfterRemovingAPokemon(removedPokemon) {
-        let pokemonList = this.state.pokemonList.filter((pokemon) => { return pokemon !== removedPokemon });
+        let pokemonList = this.state.pokemonList.filter((pokemon) => { return pokemon.name !== removedPokemon });
 
         this.setState({
             pokemonList: pokemonList
@@ -29,22 +43,35 @@ export default class MyPokemonList extends React.Component {
     }
 
     render() {
-        const pokemons = this.state.pokemonList.map((pokemon) =>
-            <React.Fragment key={pokemon}>
-                <Link 
-                to={`/pokemon/${pokemon}`}><li>{pokemon}</li></Link>
-
-                <RemovePokemonFromList name={pokemon} updateMyListAfterRemovingAPokemon={this.updateMyListAfterRemovingAPokemon} />
-            </React.Fragment>
-        );
+        const { classes } = this.props;
+        const pokemons = this.state.pokemonList.map((link) => {
+            console.log(link.name);
+            return (
+                <ListItem key={`/pokemon/${link.name}`} dense button className={classes.listItem}>
+                    <Avatar alt={link.name} src={link.photo} />
+                    <Link className="pokemon-details-link" to={`/pokemon/${link.name}`}><ListItemText primary={`${link.name}`} />
+                        <ListItemSecondaryAction>
+                            <MyPokemonCheckbox 
+                            name={link.name} 
+                            pokemonPhoto={link.photo}
+                            checked={this.state.checked} 
+                            updateMyListAfterRemovingAPokemon={this.updateMyListAfterRemovingAPokemon}
+                            />
+                        </ListItemSecondaryAction>
+                    </Link>
+                </ListItem>
+            );
+        })
 
         return (
-            <React.Fragment>
+            <div className={`${classes.root}`} id="my-pokemon-list">
                 <h1>My Pokemon List Goes Here!</h1>
-    
-                <hr />
-                {pokemons}
-            </React.Fragment>
+                <List>
+                    {pokemons}
+                </List>
+            </div>
         );
     }
 }
+
+export default withStyles(styles)(MyPokemonList);
